a7aed608dec528c1b3106d16cdd84796c3983171
saskianovalia <plg20238556.siswa@smkn12malang.sch.id>
2025-04-22 07:25:45 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,14 +1,78 @@
-import 'package:acakdadu/gradient_container.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(
-          Color.fromARGB(255, 46, 19, 63), Color.fromARGB(255, 139, 24, 159)
-         ),
+void main() {
+  runApp(const MaterialApp(title: 'Navigation Basics', home: FirstRoute()));
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('REGISTER')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+             
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                ),
+              ),
+              const SizedBox(height: 16), 
+              
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+                obscureText: true, 
+              ),
+              const SizedBox(height: 16), 
+              ElevatedButton(
+                child: const Text('Login'),
+                onPressed: () {
+                  
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SecondRoute()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Route')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Logout!'),
+        ),
+      ),
+    );
+  }
 }
